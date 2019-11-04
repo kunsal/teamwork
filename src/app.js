51e5a9f37a71c3d00cb191dc6d8e34684a05d17b
Olakunle Salami <kunsal2003@gmail.com>
2019-11-04 15:41:23 +0000

    chore(express):delete index.js and add server bootup code in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 // Dependencies import
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const winston = require('winston');
+const logger = require('./helpers/logger');
 
 // Initialize app
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+winston.error('Please this should fail and create files');
+logger();
+const por = process.env.PORT || 3000; 
+const server = app.listen(port, winston.info(`Listening on port ${port}`));
 
-module.exports = app;
+module.exports = server;
