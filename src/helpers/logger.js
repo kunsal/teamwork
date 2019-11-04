f03b45ed7681ff6807e4ea7b474e5ca9ed8a560a
Olakunle Salami <kunsal2003@gmail.com>
2019-11-04 16:17:06 +0000

    chore(logging): finish logging implementation

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -2,26 +2,27 @@ const winston = require('winston');
 require('express-async-errors');
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-      new winston.transports.File({
-        filename: 'logs/error.log',
-        level: 'error',
-      }),
-      new winston.transports.File({ filename: 'logs/combined.log' }),
-    ],
-    exceptionHandlers: [
-      new winston.transports.File({ filename: 'logs/exceptions.log' }),
-    ],
-  });
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'logs/exceptions.log' }),
+  ],
+});
 
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(
-      new winston.transports.Console({
-        format: winston.format.simple(),
-    }));
-  };
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  );
+}
 
-  module.exports = logger;
+module.exports = logger;
