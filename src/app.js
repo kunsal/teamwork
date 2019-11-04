1b23f3f780798a2c0bed37fed2e775dc2c481895
Olakunle Salami <kunsal2003@gmail.com>
2019-11-04 22:01:27 +0000

    chore(express):remove logger test error from app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const logger = require('./helpers/logger');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-logger.error('Please this should fail and create files');
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, logger.info(`Listening on port ${port}`));
