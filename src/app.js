2eee28d95ba324cb6c34d775d291c3d6e9e442d6
Olakunle Salami <kunsal2003@gmail.com>
2019-11-04 16:02:15 +0000

    rewrite logging functionality

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 // Dependencies import
 require('dotenv').config();
 const express = require('express');
-const winston = require('winston');
 const logger = require('./helpers/logger');
 
 // Initialize app
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-winston.error('Please this should fail and create files');
-logger();
-const por = process.env.PORT || 3000; 
-const server = app.listen(port, winston.info(`Listening on port ${port}`));
+logger.error('Please this should fail and create files');
+
+const port = process.env.PORT || 3000; 
+const server = app.listen(port, logger.info(`Listening on port ${port}`));
 
 module.exports = server;
--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,28 +1,27 @@
-import winston from 'winston';
-import 'express-async-errors';
+const winston = require('winston');
+require('express-async-errors');
 
-module.exports = () => {
-  winston.createLogger({
+const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     defaultMeta: { service: 'user-service' },
     transports: [
       new winston.transports.File({
-        filename: '../../logs/error.log',
+        filename: 'logs/error.log',
         level: 'error',
       }),
-      new winston.transports.File({ filename: '../../logs/combined.log' }),
+      new winston.transports.File({ filename: 'logs/combined.log' }),
     ],
     exceptionHandlers: [
-      new winston.transports.File({ filename: '../../logs/exceptions.log' }),
+      new winston.transports.File({ filename: 'logs/exceptions.log' }),
     ],
   });
 
   if (process.env.NODE_ENV !== 'production') {
-    winston.add(
+    logger.add(
       new winston.transports.Console({
         format: winston.format.simple(),
-      }),
-    );
-  }
-};
+    }));
+  };
+
+  module.exports = logger;
