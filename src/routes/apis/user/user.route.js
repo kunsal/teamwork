d9ff220090c1d5ec2048a819c12dcde6b0a801b4
Olakunle Salami <kunsal2003@gmail.com>
2019-11-06 15:03:34 +0000

    feature(auth): add auth middleware to create user route

--- src/routes/apis/user/user.route.js
+++ src/routes/apis/user/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userController = require('../../../controllers/apis/user.controller');
 const authController = require('../../../controllers/apis/auth.controller');
+const authMiddleware = require('../../../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/create-user', userController.create);
+router.post('/create-user', [authMiddleware], userController.create);
 router.post('/signin', authController.login);
 
 module.exports = router;
