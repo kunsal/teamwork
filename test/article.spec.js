aedd2d714b15cccf7485f2f1824ff70b9e5a49b4
Olakunle Salami <kunsal2003@gmail.com>
2019-11-13 12:29:22 +0100

    chore(test-coverage):raise article test

--- test/article.spec.js
+++ test/article.spec.js
@@ -1,14 +1,31 @@
 const { chai, expect, app } = require('./base.spec');
 const articleController = require('../src/controllers/apis/article.controller');
+const ArticleModel = require('../src/models/article.model');
+const sinon = require('sinon');
+
+const Article = new ArticleModel;
+
+let jwtToken;
+//let userData = 
 
 describe('article', () => {
   const url = '/api/v1';
   describe('POST /', () => {
+    beforeEach(() => {
+      jwtToken = 'aNewParticularlongString';
+      sinon.stub(Article, 'create').returns({rowCount: 1});
+    });
+
+    afterEach(() => {
+      Article.create.restore();
+    });
+
     it('create should exist as a function', (done) => {
       expect(articleController.create).to.be.a('function');
       done();
     });
 
+    
     it('should be accessible at /articles', () => {
       chai.request(app)
         .post(`${url}/articles`)
@@ -17,9 +34,36 @@ describe('article', () => {
           expect(res.status).to.not.equal(404);
         });
     });
+
+    it('should not create article if user is not logged in', () => {
+      chai.request(app)
+        .post(`${url}/articles`)
+        .send({
+          title: "Test article 1",
+          article: "This is a pretty long article and the simplicity does not counter its longeivity",
+          tags: "Nigeria, home, land"
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+        });
+    });
+
+    // it('should create article if user is logged in', () => {
+    //   chai.request(app)
+    //     .post(`${url}/articles`)
+    //     .set('Authorization', `Bearer ${jwtToken}`)
+    //     .send({
+    //       title: "Test article 1",
+    //       article: "This is a pretty long article and the simplicity does not counter its longeivity",
+    //       tags: "Nigeria, home, land"
+    //     })
+    //     .end((err, res) => {
+    //       expect(res.status).to.equal(201);
+    //     });
+    // });
   });
 
-  /* describe('GET single article', () => {
+ /*describe('GET single article', () => {
     it('single method should exist as a function', () => {
       expect(articleController.single).to.be.a('function');
     });
@@ -40,6 +84,7 @@ describe('article', () => {
     
     it('should be accessible at /articles/:id', () => {
       chai.request(app)
+        .set('Authorization', `Bearer ${jwtToken}`)
         .delete(`${url}/articles/1`)
         .end((err, res) => {
           expect(res.status).to.not.equal(404);
