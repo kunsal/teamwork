c040dc1f818033b6b660be3ee15475b9eea2bdee
Olakunle Salami <kunsal2003@gmail.com>
2019-11-27 22:15:51 +0100

    bugfix(login):add email to login response

--- src/controllers/apis/auth.controller.js
+++ src/controllers/apis/auth.controller.js
@@ -13,13 +13,13 @@ module.exports.login = async (req, res) => {
     if (user.rowCount < 1) return errorResponse(res, loginError);
     const currentUser = user.rows[0];
     const {
-      id, password, isadmin, firstname, lastname,
+      id, password, isadmin, firstname, lastname, email
     } = currentUser;
     const validPassword = await userModel.verify(req.body.password, password);
     if (!validPassword) return errorResponse(res, loginError);
     const token = userModel.generateAuthToken(currentUser);
     const data = {
-      token, isAdmin: isadmin, firstName: firstname , lastName: lastname, userId: id,
+      token, userId: id, email, isAdmin: isadmin, firstName: firstname , lastName: lastname
     };
     res.send(response.success(data));
   } catch (e) {
